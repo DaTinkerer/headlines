a0e6501cf68f7a44e873126d9eb87721b13d16b3
bryant <bryant>
2022-05-01 23:30:14 -0400

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,65 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import BreakingView from "../views/BreakingView.vue";
+import BusinessView from "../views/BusinessView.vue";
+import EntertainmentView from "../views/EntertainmentView.vue";
+import HealthView from "../views/HealthView.vue";
+import NationView from "../views/NationView.vue";
+import ScienceView from "../views/ScienceView.vue";
+import SportsView from "../views/SportsView.vue";
+import WorldView from "../views/WorldView.Vue";
+import TechView from "../views/TechView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "breaking",
+    component: BreakingView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/business",
+    name: "business",
+    component: BusinessView,
+  },
+  {
+    path: "/entertainment",
+    name: "entertainment",
+    component: EntertainmentView,
+  },
+  {
+    path: "/health",
+    name: "health",
+    component: HealthView,
+  },
+  {
+    path: "/nation",
+    name: "nation",
+    component: NationView,
+  },
+  {
+    path: "/science",
+    name: "science",
+    component: ScienceView,
+  },
+  {
+    path: "/sports",
+    name: "sports",
+    component: SportsView,
+  },
+  {
+    path: "/tech",
+    name: "tech",
+    component: TechView,
+  },
+  {
+    path: "/world",
+    name: "world",
+    component: WorldView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/BreakingView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="breaking">
+    <section class="news-list" v-for="article in articles" :key="article.id">
+      <p>{{ article.description }}</p>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "BreakingView",
+
+  data() {
+    return {
+      articles: []
+    };
+  },
+
+  async created () {
+    this.getNews()
+  },
+  methods: {
+    async getNews() {
+      axios
+        .get(
+          "https://gnews.io/api/v4/top-headlines?token=XXXXX&lang=en"
+        )
+        .then((res) => {
+          this.articles = res.data.articles
+
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
